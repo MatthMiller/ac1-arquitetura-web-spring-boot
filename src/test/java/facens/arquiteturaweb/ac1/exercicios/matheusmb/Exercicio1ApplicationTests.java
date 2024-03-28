665d6e53d842e4276987af086c9b6fe5b6b0b2ad
MatthMiller <matheus.monteiro.genesis@gmail.com>
2024-03-28 20:10:55 -0300

    Commit da primeira atividade

---
+++ src/test/java/facens/arquiteturaweb/ac1/exercicios/matheusmb/Exercicio1ApplicationTests.java
@@ -0,0 +1,13 @@
+package facens.arquiteturaweb.ac1.exercicios.matheusmb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercicio1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
